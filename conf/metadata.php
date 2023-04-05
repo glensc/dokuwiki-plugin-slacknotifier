d82e19bdf37749bdf88da98b886170792ddd981a
Elan Ruusamäe <glen@delfi.ee>
2023-04-05 21:07:45 +0300

    Minor: Reformat config files with sane ident

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,7 +1,8 @@
 <?php
-        $meta['namespaces'] = array('string');
-        $meta['notify_create'] = array('onoff');
-        $meta['notify_edit'] = array('onoff');
-        $meta['notify_delete'] = array('onoff');
-        $meta['show_summary'] = array('onoff');
-        $meta['webhook'] = array('string');
+
+$meta['namespaces'] = array('string');
+$meta['notify_create'] = array('onoff');
+$meta['notify_edit'] = array('onoff');
+$meta['notify_delete'] = array('onoff');
+$meta['show_summary'] = array('onoff');
+$meta['webhook'] = array('string');
