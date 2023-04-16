6bec4d4b7f37c43d95edfc6a7c3eafaa1e827024
Elan Ruusamäe <glen@delfi.ee>
2023-04-16 16:51:47 +0300

    Add BaseEvent class

---
+++ event/BaseEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace dokuwiki\plugin\slacknotifier\event;
+
+use dokuwiki\Extension\Event;
+use InvalidArgumentException;
+
+abstract class BaseEvent
+{
+    /** @var array */
+    private $data;
+
+    public function __construct(Event $event) {
+        $this->data = $event->data;
+    }
+
+    public function __get(string $name)
+    {
+        if (!array_key_exists($name, $this->data)) {
+            throw new InvalidArgumentException("Invalid property: $name");
+        }
+
+        return $this->data[$name];
+    }
+}
